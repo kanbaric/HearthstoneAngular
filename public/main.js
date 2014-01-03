17587f2b5462be023a1a8e6636b5366f804dbe04
kanbaric <kanbaric@gmail.com>
2014-01-03 15:23:13 -0800

    Refactored card data service to use a differently structured, and more up-to-date cards.json source.

--- public/main.js
+++ public/main.js
@@ -1,68 +1,136 @@
 var Hearthstone = angular.module('Hearthstone', []);
-Hearthstone.factory('cardsService',['$http',  function($http){
-    var cardsService = {
-        getCards: function(cb){
-                return $http.get('/cards.json').success(cb);          
-        },
 
-        cardColumns :  [
-		    'Name',
-		    'Class',
-		    'Rarity',
+Hearthstone.factory('cardsService',[
+    '$http',
+    function($http) {
+        'use strict';
+
+        //cached promise
+        var hearthstoneData = null;
+
+        function getHearthstoneData() {
+            if (!hearthstoneData) {
+                hearthstoneData = $http.get('/cards.json').then(function (response) {
+                    //console.log(response);
+                    return response.data;
+                });
+            }
+
+            return hearthstoneData;
+        }
+
+        function getCards() {
+            return getHearthstoneData().then(function (data) {
+                return data.cards;
+            });
+        }
+
+        function getTypes() {
+            return getHearthstoneData().then(function (data) {
+                return data.labels.types
+            });
+        }
+
+        function getClasses() {
+            return getHearthstoneData().then(function (data) {
+                return data.labels.classes;
+            });
+        }
+
+        function getRaces() {
+            return getHearthstoneData().then(function (data) {
+                return data.labels.races;
+            });
+        }
+
+        function getSets() {
+            return getHearthstoneData().then(function (data) {
+                return data.labels.sets;
+            });
+        }
+
+        function getQualities() {
+            return getHearthstoneData().then(function (data) {
+                return data.labels.quality;
+            });
+        }
+
+        return {
+            getCards: getCards,
+            getTypes: getTypes,
+            getClasses: getClasses,
+            getRaces: getRaces,
+            getSets: getSets,
+            getQualities: getQualities
+        }
+    }]);
+
+
+Hearthstone.controller('CardsCtrl',[
+    '$scope',
+    'cardsService',
+    function ($scope, cardsService) {
+        'use strict';
+
+        $scope.sort = {};
+        $scope.columns = [
+            'Name',
+            'Class',
+            'Rarity',
             'Type',
             'Race',
             'Cost',
             'Attack',
             'Health',
             'Description'
-	    ],
+        ];
 
-        cardTypes : [
-            'Spell', 
-            'Minion', 
-            'Weapon'
-        ],
+        cardsService.getCards().then(function(cards) {
+            $scope.cards = cards;
+        });
 
-        cardClasses: [
-            'Priest',
-            'Druid',
-            'Hunter',
-            'Rogue',
-            'Paladin',
-            'Warrior',
-            'Warlock',
-            'Shaman',
-            'Mage'
-	    ]
-    };
-    
-    return cardsService; 
-}]);
-Hearthstone.controller('CardsCtrl', ['$scope', '$http','cardsService', function ($scope, $http, cardsService) {
-	$scope.sort = {};
-	$scope.columns = cardsService.cardColumns;
-    $scope.types = cardsService.cardTypes;
-    $scope.classes = cardsService.cardClasses;
-    cardsService.getCards(function(data){
-		$scope.cards = data;
-	});
+        cardsService.getTypes().then(function(types) {
+            $scope.types = types;
+            $scope.types[0] = '';
+        });
 
-	$scope.setClass = function(theClass){
-		$scope.theClass = theClass;
-	};
-	$scope.setType = function(theType){
-		$scope.theType = theType;
-	};
-	$scope.setOrderBy = function(column){
-		var sort = $scope.sort;
-        if (sort.column == column.toLowerCase()) {
-            sort.descending = !sort.descending;
-        } else {
-            sort.column = column.toLowerCase();
-            sort.descending = false;
-        }
-	};
-}]);
+        cardsService.getClasses().then(function(classes) {
+            $scope.classes = classes;
+        });
+
+        cardsService.getRaces().then(function(races) {
+            $scope.races = races;
+            $scope.races[0] = '';
+        });
+
+        cardsService.getSets().then(function(sets) {
+            $scope.sets = sets;
+            $scope.sets[0] = '';
+        });
+
+        cardsService.getQualities().then(function(qualities) {
+            $scope.qualities = qualities;
+        });
+
+
+        $scope.setClass = function(theClass){
+            $scope.theClass = theClass;
+        };
+
+        $scope.setType = function(theType){
+            $scope.theType = theType;
+        };
+
+        $scope.setOrderBy = function(column){
+            var sort = $scope.sort;
+            if (sort.column == column.toLowerCase()) {
+                sort.descending = !sort.descending;
+            } else {
+                sort.column = column.toLowerCase();
+                sort.descending = false;
+            }
+        };
+    }]);
 
 Hearthstone.directive('cardsTable', function(){
     return {
