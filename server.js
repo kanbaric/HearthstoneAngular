1209446ea2ddd35c28f3257fcdf53e85a027ba08
kanbaric <kanbaric@gmail.com>
2014-01-03 15:22:03 -0800

    Sanitize express app, updated gitignore.

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var http = require('http');
 
 var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(__dirname+'/public'));
-app.listen(3333);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
